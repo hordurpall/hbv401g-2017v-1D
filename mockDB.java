a07c5624dc78ac76d486562970238d2fafe35d7a
ebh17 <ebh17@hi.is>
2017-03-31 11:49:39 +0000

    ekki lengur array í mock

---
+++ mockDB.java
@@ -0,0 +1,18 @@
+package trunk;
+public mockDB() {
+}
+
+public ArrayList<Trip> searchForKeyword(String query) throws ConnectException {
+
+	List<Trip> Trip_list = new List<Trip>();
+	Trip trip1 = new Trip("Golden Cirlce");
+	Trip trip2 = new Trip("Blue lagon");
+	Trip trip3 = new Trip("Horse riding");
+	trip_list.add(trip1);
+	trip_list.add(trip2);
+	trip_list.add(trip3);
+	
+	retrun trip_list;
+
+	}
+}
