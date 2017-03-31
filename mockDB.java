41e69fc85aaa7d0bc9fdd8e5b59890505fdeebfb
ebh17 <ebh17@hi.is>
2017-03-31 11:52:47 +0000

    .

--- mockDB.java
+++ mockDB.java
@@ -8,9 +8,9 @@ public List<Trip> searchForKeyword(String query) throws ConnectException {
 	Trip trip1 = new Trip("Golden Cirlce");
 	Trip trip2 = new Trip("Blue lagon");
 	Trip trip3 = new Trip("Horse riding");
-	trip_list.add(trip1);
-	trip_list.add(trip2);
-	trip_list.add(trip3);
+	Trip_list.add(trip1);
+	Trip_list.add(trip2);
+	Trip_list.add(trip3);
 	
 	retrun trip_list;
 
