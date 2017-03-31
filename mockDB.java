e04822e38210c0f29b2d44b3885664cbf1246214
ebh17 <ebh17@hi.is>
2017-03-31 12:24:25 +0000

    í raun ekkert

--- mockDB.java
+++ mockDB.java
@@ -1,18 +1,19 @@
 package trunk;
+
 public mockDB() {
-}
 
-public List<Trip> searchForKeyword(String query) throws ConnectException {
 
-	List<Trip> trip_list = new List<Trip>();
-	Trip trip1 = new Trip("Golden Cirlce");
-	Trip trip2 = new Trip("Blue lagon");
-	Trip trip3 = new Trip("Horse riding");
-	trip_list.add(trip1);
-	trip_list.add(trip2);
-	trip_list.add(trip3);
-	
-	return trip_list;
+	public List<Trip> searchForKeyword(String query) throws ConnectException {
 
+		List<Trip> trip_list = new List<Trip>();
+		Trip trip1 = new Trip("Golden Cirlce");
+		Trip trip2 = new Trip("Blue Lagon");
+		Trip trip3 = new Trip("Horse riding");
+		trip_list.add(trip1);
+		trip_list.add(trip2);
+		trip_list.add(trip3);
+		
+		return trip_list;
+
+		}
 	}
-}
